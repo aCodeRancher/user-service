1fa67183d25a445166cd2bfb67b017adc9ea4773
helen <hhma@alumni.cpp.edu>
2020-09-26 23:36:15 -0400

    remove HttpStatus import

--- src/test/java/com/codespacelab/user/UserApplicationTests.java
+++ src/test/java/com/codespacelab/user/UserApplicationTests.java
@@ -11,7 +11,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
