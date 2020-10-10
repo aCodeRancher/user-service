28f08d036d7979b4e6c24bd2704bfe4302172132
helen <hhma@alumni.cpp.edu>
2020-10-09 20:48:29 -0400

    remove security features.

--- src/main/java/com/codespacelab/user/config/UserWebSecurityConfig.java
+++ src/main/java/com/codespacelab/user/config/UserWebSecurityConfig.java
@@ -1,14 +1,14 @@
 package com.codespacelab.user.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-@Configuration
-public class UserWebSecurityConfig extends WebSecurityConfigurerAdapter{
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//@Configuration
+/*public class UserWebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure (HttpSecurity httpSecurity) throws Exception{
         httpSecurity.csrf().disable()
                 .authorizeRequests()
                .anyRequest().authenticated().and().httpBasic();
     }
-}
+}*/
